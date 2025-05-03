6966626db4cd6ef93f0141bf57e0574f76b07ad8
Oleksii <alexey.bortnytskyi@gmail.com>
2025-05-03 18:23:04 +0200

    feature(actions/data) - created <Categories> options

---
+++ actions/(data)/categories.actions.ts
@@ -0,0 +1,74 @@
+export const categories = [
+    {
+        label: "Frontend",
+        value: "frontend",
+    },
+    {
+        label: "Backend",
+        value: "backend",
+    },
+    {
+        label: "DevOps",
+        value: "devops",
+    },
+    {
+        label: "Database",
+        value: "database",
+    },
+    {
+        label: "Cloud",
+        value: "cloud",
+    },
+    {
+        label: "Mobile",
+        value: "mobile",
+    },
+    {
+        label: "Data Science",
+        value: "data_science",
+    },
+    {
+        label: "Machine Learning",
+        value: "machine_learning",
+    },
+    {
+        label: "AI",
+        value: "ai",
+    },
+    {
+        label: "Security",
+        value: "security",
+    },
+    {
+        label: "Networking",
+        value: "networking",
+    },
+    {
+        label: "Game Development",
+        value: "game_development",
+    },
+    {
+        label: "UI/UX Design",
+        value: "ui_ux_design",
+    },
+    {
+        label: "Project Management",
+        value: "project_management",
+    },
+    {
+        label: "Business Analysis",
+        value: "business_analysis",
+    },
+    {
+        label: "Other",
+        value: "other",
+    },
+    {
+        label: "Soft Skills",
+        value: "soft_skills",
+    },
+    {
+        label: 'Principles',
+        value: 'principles',
+    }
+]
